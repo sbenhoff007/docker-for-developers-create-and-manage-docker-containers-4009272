2a74197094459a1bf01179093502ca3d10f9749d
Shelley Benhoff <shelley.benhoff@gmail.com>
2025-07-02 15:52:55 -0400

    Updated app and added compose.yml

--- app/index.js
+++ app/index.js
@@ -1,10 +1,37 @@
-// app/index.js
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { getMessage, setMessage, initDB } = require('./db');
 
-const server = http.createServer((req, res) => {
-  res.end('Hello from Docker!');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', async (req, res) => {
+  const message = await getMessage();
+  res.send(`
+    <html>
+      <body>
+        <h1>${message}</h1>
+        <form method="POST" action="/update">
+          <input type="text" name="message" placeholder="New message" />
+          <button type="submit">Update</button>
+        </form>
+      </body>
+    </html>
+  `);
 });
 
-server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.post('/update', async (req, res) => {
+  await setMessage(req.body.message);
+  res.redirect('/');
 });
+
+initDB()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server listening on port 3000');
+    });
+  })
+  .catch(err => {
+    console.error('Database initialization failed:', err);
+    process.exit(1);
+  });
